02d8e2d48cdfe69950cc6fe4a4697e2a7d644155
mhalfmann <mhalfmann1978@gmail.com>
2021-03-22 14:23:40 +0100

    second app

--- Apps/Museum2D/assets/images.js
+++ Apps/Museum2D/assets/images.js
@@ -1,160 +1,160 @@
 let images = {
     Gm903:{
-        'file':"./assets/images/NorthWallImages/Gm903_cut.jpg",
+        'file':"../assets/images/NorthWallImages/Gm903_cut.jpg",
         'id':"Gm903",
         'height':48.1,
         'width': 36.8,
         'text':"Ein Putto rafft den Vorhang und gibt den Blick frei auf die demütig kniende Gottesmutter. Das Gemälde verbindet das Motiv inniger Mutterliebe mit der Erkenntnis der künftigen Passion Christi. Diese gewinnt Maria aus den Schriften der Propheten, die ein Putto aufblättert."
     },
     PlO156:{
-        'file':"./assets/images/NorthWallImages/Pl.O.156_cut.png",
+        'file':"../assets/images/NorthWallImages/Pl.O.156_cut.png",
         'id':"PlO156",
         'height':66.5,
         'width': 59,
         'text':"Die Flucht ist der Legenda Aurea entsprechend geschildert. Ein Palmzweig wird durch Engel herabgebogen. Josef erntet die Früchte, Maria und ihr Kind essen davon. Der Bildhauer nutzte einen Kupferstich Martin Schongauers als Vorlage für seine Komposition."
     },
     Gm344:{
-        'file':"./assets/images/NorthWallImages/Gm344_cut.jpg",
+        'file':"../assets/images/NorthWallImages/Gm344_cut.jpg",
         'id':"Gm344",
         'height':48.6,
         'width': 38.8,
         'text':"Gezeigt ist eine Episode der Kindheit Christi. Die einem Familienidyll angenäherte Szene nimmt eine Sonderstellung im Werk Baldungs ein. Pittoreske Naturschilderung und harmonische Stimmung bedienen den exklusiven Geschmack eines Kunstliebhabers."
     },
     Gm1170:{
-        'file':"./assets/images/NorthWallImages/Gm1170_cut.jpg",
+        'file':"../assets/images/NorthWallImages/Gm1170_cut.jpg",
         'id':"Gm1170",
         'height':91.5,
         'width': 63.3,
         'text':"Baldungs Version des traditionellen Motivs der »Stillenden Maria« changiert gewagt zwischen religiösem Bekenntnisbild und sinnlich-erotischer Darstellung. Im demonstrativen Halten der Brustwarze wird die Menschwerdung Christi betont, die 1533 auf der Straßburger Synode zentrales Thema war. Die makellose Schönheit und die Handhaltung Marias spielt andererseits auf Bilder der Venus an. Das Gemälde erhält damit eine für Baldung typische Mehrdeutigkeit, die der gelehrte Kunstkenner zu schätzen wusste."
     },
     PlO2206:{
-        'file':"./assets/images/NorthWallImages/Pl.O.2206_cut.png",
+        'file':"../assets/images/NorthWallImages/Pl.O.2206_cut.png",
         'id':"PlO2206",
         'height':150,
         'width': 58,
         'text':"Die Werke des Meisters, der am Hochaltar des Münsters zu Breisach sein Monogramm 'HL' hinterließ, zählen zu den bizarrsten Schöpfungen deutscher Kunst. Vielleicht ist dieser Bildhauer mit einem in Freiburger Quellen genannten Hans Loy zu identifizieren. Hervorstechendes Merkmal seiner Figuren ist das plastisch-graphische Eigenleben der Gewandmassen. Dem vielfältig variierten und eigentümlich stilisierten Faltenwurf des Evangelisten wohnt heftige Bewegung inne. Sie verleiht ihm visionäre und ekstatische Züge."
     },
     PlO2205:{
-        'file':"./assets/images/NorthWallImages/Pl.O.2205_cut.png",
+        'file':"../assets/images/NorthWallImages/Pl.O.2205_cut.png",
         'id':"PlO2205",
         'height':150,
         'width': 55,
         'text':"Das Zusammenspiel des ausgemergelten, muskulösen Körpers, des knochigen Gesichtes und des nervös geknitterten Gewandes spiegelt die innere Unruhe des Täufers wider. Die Darstellung beschreibt eindrucksvoll den in der Wüste lebenden Asketen, der sich von Honig und Heuschrecken ernährte. Das Lamm symbolisiert Christus, der als unschuldiges Opfer die Sünden der Welt auf sich genommen und gesühnt hat. Die Figuren der beiden hll. Johannes waren gemeinsam in einem Flügelaltar aufgestellt."
     },
     Gm1000:{
-        'file':"./assets/images/NorthWallImages/Gm1000_cut.jpg",
+        'file':"../assets/images/NorthWallImages/Gm1000_cut.jpg",
         'id':"Gm1000",
         'height':99,
         'width': 68,
         'text':"In Kontrast zum innigen Thema der Muttergottes, die ihr Kind liebkost, steht die artifizielle, kühle Farbigkeit. Wie oft bei Baldung haftet dem Bild etwas Rätselhaftes an. Das Motiv folgt einem Gemälde Mabuses, auf dem jedoch die schwierig zu deutenden Edelsteine fehlen."
     },
     Gm338:{
-        'file':"./assets/images/NorthWallImages/Gm338_cut.jpg",
+        'file':"../assets/images/NorthWallImages/Gm338_cut.jpg",
         'id':"Gm338",
         'height':106.5,
         'width': 63.4,
         'text':"Beide Gemälde stammen von einem Altar, der dem Bildersturm zum Opfer fiel. Neun Tafeln blieben erhalten. Sie zeigen Szenen aus dem Leben Marias und orientieren sich an Holzschnitten Albrecht Dürers. Fries setzte die Vorlagen selbständig und erfindungsreich um. Der in Bern, Basel und Fribourg tätige Maler zählt neben Niklaus Manuel und Urs Graf zu den markantesten Schweizer Künstlern der Zeit vor der Reformation. In seinem Werk verbindet sich spätgotisches Formempfinden mit eindringlichem Naturalismus."
     },
     Gm339:{
-        'file':"./assets/images/NorthWallImages/Gm339_cut.jpg",
+        'file':"../assets/images/NorthWallImages/Gm339_cut.jpg",
         'id':"Gm339",
         'height':107,
         'width': 56,
         'text':"Beide Gemälde stammen von einem Altar, der dem Bildersturm zum Opfer fiel. Neun Tafeln blieben erhalten. Sie zeigen Szenen aus dem Leben Marias und orientieren sich an Holzschnitten Albrecht Dürers. Fries setzte die Vorlagen selbständig und erfindungsreich um. Der in Bern, Basel und Fribourg tätige Maler zählt neben Niklaus Manuel und Urs Graf zu den markantesten Schweizer Künstlern der Zeit vor der Reformation. In seinem Werk verbindet sich spätgotisches Formempfinden mit eindringlichem Naturalismus."
     },
     Gm1079L:{
-        'file':"./assets/images/EastWallImages/Gm1079L_cut.jpg",
+        'file':"../assets/images/EastWallImages/Gm1079L_cut.jpg",
         'id':"GmGm1079L1000",
         'height':121.4,
         'width': 32,
         'text':"Ab 1503 war Baldung in Nürnberg unter dem Einfluss Dürers tätig. Der Altar ist jedoch ein eigenständiges, signiertes Werk. Der junge Maler erscheint im Selbstbildnis auf der Mitteltafel im grünen Gewand, das auf seinen Übernamen anspielt. In Oberflächenbehandlung und Farbigkeit setzt Baldung eigenwillige Akzente, mit denen er sich von Dürer löst. Der Altar war wohl ein Auftrag des Erzbischofs Ernst von Magdeburg für die Maria-Magdalenen-Kapelle auf der Moritzburg in Halle, die er bis 1503 als neue Residenz errichten ließ."
     },
     Gm1079M:{
-        'file':"./assets/images/EastWallImages/Gm1079M_cut.jpg",
+        'file':"../assets/images/EastWallImages/Gm1079M_cut.jpg",
         'id':"Gm1079M",
         'height':121.4,
         'width': 78.7,
         'text':"Ab 1503 war Baldung in Nürnberg unter dem Einfluss Dürers tätig. Der Altar ist jedoch ein eigenständiges, signiertes Werk. Der junge Maler erscheint im Selbstbildnis auf der Mitteltafel im grünen Gewand, das auf seinen Übernamen anspielt. In Oberflächenbehandlung und Farbigkeit setzt Baldung eigenwillige Akzente, mit denen er sich von Dürer löst. Der Altar war wohl ein Auftrag des Erzbischofs Ernst von Magdeburg für die Maria-Magdalenen-Kapelle auf der Moritzburg in Halle, die er bis 1503 als neue Residenz errichten ließ."
     },
     Gm1079R:{
-        'file':"./assets/images/EastWallImages/Gm1079R_cut.jpg",
+        'file':"../assets/images/EastWallImages/Gm1079R_cut.jpg",
         'id':"Gm1079R",
         'height':121.4,
         'width': 32,
         'text':"Ab 1503 war Baldung in Nürnberg unter dem Einfluss Dürers tätig. Der Altar ist jedoch ein eigenständiges, signiertes Werk. Der junge Maler erscheint im Selbstbildnis auf der Mitteltafel im grünen Gewand, das auf seinen Übernamen anspielt. In Oberflächenbehandlung und Farbigkeit setzt Baldung eigenwillige Akzente, mit denen er sich von Dürer löst. Der Altar war wohl ein Auftrag des Erzbischofs Ernst von Magdeburg für die Maria-Magdalenen-Kapelle auf der Moritzburg in Halle, die er bis 1503 als neue Residenz errichten ließ."
     },
     Gm1108:{
-        'file':"./assets/images/SouthWallImages/Gm1108_cut.jpg",
+        'file':"../assets/images/SouthWallImages/Gm1108_cut.jpg",
         'id':"Gm1108",
         'height':75.5,
         'width': 117.4,
         'text':"Das Gemälde zeigt eine seit dem Spätmittelalter häufig thematisierte 'Weiberlist'. Eine des Ehebruchs angeklagte Frau legt die Hand in den Löwenrachen. Sie bekennt, nur in den Armen ihres Ehemanns und des Narren gelegen zu haben. Da sie die Wahrheit spricht, beißt der Löwe nicht zu. Der Narr ist in Wirklichkeit ihr verkleideter Liebhaber. Von den Zeugen wird er jedoch nicht ernst genommen. Die Szene spielt auf die römische Steinmaske 'bocca della veritá' an, die der Legende nach jedem Lügner die Hand abbeißt."
     },
     Gm218:{
-        'file':"./assets/images/SouthWallImages/Gm218_cut.jpg",
+        'file':"../assets/images/SouthWallImages/Gm218_cut.jpg",
         'id':"Gm218",
         'height':86.7,
         'width': 58.5,
         'text':"Der verliebte Greis wird Opfer einer jungen Frau. Durchtrieben blickt sie zum Betrachter und macht ihn zum Komplizen. Das Thema des 'Ungleichen Paares' wurde in vielen Varianten dargestellt. Es ermahnte den Betrachter zur Beherrschung seiner Triebe."
     },
     Gm47:{
-        'file':"./assets/images/SouthWallImages/Gm47_cut.jpg",
+        'file':"../assets/images/SouthWallImages/Gm47_cut.jpg",
         'id':"Gm47",
         'height':71,
         'width': 53.8,
         'text':"Das Werk ist eine Variante von Leonardos Mona Lisa als Halbakt. Umstritten ist, ob die »nackte Gioconda« ein Bildnis der Geliebten des Kölner Erzbischofs war. Die Darstellung gehört zum Typus des Kurtisanenbildes, das sich im 16. Jahrhundert europaweit etablierte."
     },
     Gm1093:{
-        'file':"./assets/images/SouthWallImages/Gm1093_cut.jpg",
+        'file':"../assets/images/SouthWallImages/Gm1093_cut.jpg",
         'id':"Gm1093",
         'height':208.5,
         'width': 74,
         'text':"Das Gemälde zeigt die alttestamentliche Heldin Judith. Bei einem Gelage machte sie den Feldherrn Holofernes trunken und schlug ihm das Haupt ab. Dadurch rettete sie Palästina vor dem Überfall der Assyrer. Judith erscheint in heroischer Monumentalität nackt vor schwarzem Grund. Sie verkörpert die Gefahren verführerischer Schönheit ebenso wie die heldenhafte Tat. Die Tafel gehörte zu einem Zyklus und war das Pendant zum Bild der Venus. Baldung betonte damit den Aspekt der moralgefährdenden 'Weibermacht'."
     },
     Gm1097:{
-        'file':"./assets/images/SouthWallImages/Gm1097_cut.jpg",
+        'file':"../assets/images/SouthWallImages/Gm1097_cut.jpg",
         'id':"Gm1097",
         'height':175.4,
         'width': 66.3,
         'text':"Die mädchenhafte Liebesgöttin mit Amor hat Cranach in vielen Varianten gemalt. In der Körperhaltung der Venus griff der Künstler auf Formgut des 15. Jh. zurück. Sie steht für kokette Erotik, die auch im direkten Blick und im durchsichtigen Schleier zum Ausdruck kommt."
     },
     Gm213:{
-        'file':"./assets/images/SouthWallImages/Gm213_cut.jpg",
+        'file':"../assets/images/SouthWallImages/Gm213_cut.jpg",
         'id':"Gm213",
         'height':50.1,
         'width': 34.4,
         'text':"Die Liebesgöttin mit Amor ist eines der populärsten Sujets der Bildermanufaktur Cranachs. Venus wird als Kindfrau mit zarter Erotik dargestellt. Der Liebesgott Amor wird von Bienen gestochen. Das Motiv warnt vor den schmerzhaften Folgen ungezügelter Wollust."
     },
     PlO680:{
-        'file':"./assets/images/SouthWallImages/Pl.O.680_cut.jpg",
+        'file':"../assets/images/SouthWallImages/Pl.O.680_cut.jpg",
         'id':"PlO680",
         'height':37,
         'width': 35.5,
         'text':"Die Szene war Teil einer Serie zum Thema der 'Weibermacht'. Männer erliegen darin den Reizen einer Frau und brechen dann die Gebote Gottes. So lässt sich Salomo durch die heidnische Tochter eines Pharao zum Anbeten ihrer Götzen verleiten."
     },
     Gm217:{
-        'file':"./assets/images/SouthWallImages/Gm217_cut.jpg",
+        'file':"../assets/images/SouthWallImages/Gm217_cut.jpg",
         'id':"Gm217",
         'height':58.6,
         'width': 59.8,
         'text':"Auf dem unteren Teil der Tafel war das abgeschlagene Johanneshaupt abgebildet. Das abstoßende Motiv wurde abgesägt. Dadurch wurde das Bildnis zu einer Ikone altdeutscher Schönheit. Wilhelm Hauff setzte dem Gemälde 1826 in einer Novelle ein Denkmal."
     },
     Gm1086:{
-        'file':"./assets/images/WestWallImages/Gm1086_cut.jpg",
+        'file':"../assets/images/WestWallImages/Gm1086_cut.jpg",
         'id':"Gm1086",
         'height':67.4,
         'width': 52.8,
         'text':"Trotz der in der Inschrift beschworenen Naturnähe zeigt das Porträt deutlich manieristische Tendenzen: Auffallend glatte Oberflächen, scharfe Konturen und eine kühle Farbigkeit. Ziel ist nicht die Nachahmung der Natur, sondern Stilisierung und effektvolle Inszenierung."
     },
     Gm1195:{
-        'file':"./assets/images/WestWallImages/Gm1195_cut.jpg",
+        'file':"../assets/images/WestWallImages/Gm1195_cut.jpg",
         'id':"Gm1195",
         'height':57.2,
         'width': 43.1,
         'text':"Das Gemälde schließt unmittelbar an Bildnistypen Hans Holbeins an. Die übermalte erste Fassung zeigte einen kleineren Tod mit Stundenglas. Auf Wunsch des Auftraggebers akzentuierte der Maler das Motiv und ergänzte das Bildnis durch die humanistische Inschrift."
     },
     Gm1115:{
-        'file':"./assets/images/WestWallImages/Gm1115_cut.jpg",
+        'file':"../assets/images/WestWallImages/Gm1115_cut.jpg",
         'id':"Gm1115",
         'height':39.1,
         'width': 32.2,
