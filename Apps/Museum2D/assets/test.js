e8167a1bcd8334345ce0626fad368f5b7c98fab4
mhalfmann <mhalfmann1978@gmail.com>
2021-03-23 14:32:27 +0100

    updated data array

---
+++ Apps/Museum2D/assets/test.js
@@ -0,0 +1,31 @@
+/* testData = [[
+        {'file':"../assets/images/NorthWallImages/Gm903_cut.jpg",
+        'id':"Gm903",
+        'height':48.1,
+        'width': 36.8,
+        'text':"Ein Putto rafft den Vorhang und gibt den Blick frei auf die demütig kniende Gottesmutter. Das Gemälde verbindet das Motiv inniger Mutterliebe mit der Erkenntnis der künftigen Passion Christi. Diese gewinnt Maria aus den Schriften der Propheten, die ein Putto aufblättert."}
+    
+],
+[
+    {'file':"../assets/images/NorthWallImages/Gm903_cut.jpg",
+    'id':"Gm1079",
+    'height':48.1,
+    'width': 36.8,
+    'text':"Ein Putto rafft den Vorhang und gibt den Blick frei auf die demütig kniende Gottesmutter. Das Gemälde verbindet das Motiv inniger Mutterliebe mit der Erkenntnis der künftigen Passion Christi. Diese gewinnt Maria aus den Schriften der Propheten, die ein Putto aufblättert."}
+
+]] */
+
+testData = [
+    {
+        'name':'Heinz',
+        'id':'007'
+    },
+    {
+        'name':'Heinz',
+        'id':'08/15'
+    }
+]
+
+// let imageArray = [images.Gm903, images.PlO156, images.Gm344,images.Gm1170, images.PlO2206, images.PlO2205, images.Gm1000, images.Gm338,
+//     images.Gm339, images.Gm1079L, images.Gm1079M, images.Gm1079R, images.Gm1108, images.Gm218, images.Gm47, images.Gm1093, images.Gm1097,
+//     images.Gm213, images.PlO680, images.Gm217, images.Gm1086, images.Gm1195, images.Gm1115]
