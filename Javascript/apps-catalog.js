66d39cfebea79bf2b1222ef14682817a38bfb8b5
mhalfmann <mhalfmann@iwm-tuebingen.de>
2026-09-18 09:27:39 +0200

    test

--- Javascript/apps-catalog.js
+++ Javascript/apps-catalog.js
@@ -33,12 +33,12 @@
     media.className = "app-card__media";
 
     var img = document.createElement("img");
-    img.src = app.image || "./Assets/Images/FischInfo.png";
+    img.src = app.image || "./Assets/Images/Bakterien.png";
     img.alt = app.title || app.id || "App";
     img.loading = "lazy";
     img.onerror = function () {
       this.onerror = null;
-      this.src = "./Assets/Images/FischInfo.png";
+      this.src = "./Assets/Images/Bakterien.png";
     };
     media.appendChild(img);
 
